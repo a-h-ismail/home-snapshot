2b4c7c76fb9aa57feaecaf555b3cc6c0bf6a7435
Ahmad Ismail <10476735-a-h-ismail@users.noreply.gitlab.com>
2023-01-09 08:57:40 +0200

    Remove accidental comment, update script.

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ mkdir ~/.config/systemd
 mkdir ~/.config/systemd/user
 cp home-snapshot.timer home-snapshot.service ~/.config/systemd/user
 cp home-snapshot.sh ~/.local/bin
-#cp home-snapshot.conf ~/.config
-read -p "Press enter to edit your configuration file."
-nano ~/.config/home-snapshot.conf
+cp home-snapshot.conf home-snapshot-excl.conf ~/.config
+read -p "Press enter to edit your configuration and exclusion files."
+nano ~/.config/home-snapshot.conf ~/.config/home-snapshot-excl.conf
 chmod +x ~/.local/bin/home-snapshot.sh
 systemctl --user daemon-reload
 systemctl --user enable --now home-snapshot.timer

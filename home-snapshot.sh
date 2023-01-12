cbbab49f9c0833a9f7219c14be6180ba9847aa2e
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-01-12 15:30:27 +0200

    Quality improvements.

--- home-snapshot.sh
+++ home-snapshot.sh
@@ -12,23 +12,27 @@ today_date="$(date '+%d-%m-%Y')"
 backup_path="${destination_dir}/${today_date}"
 #latest link is used for hard linking snapshots to reduce used space
 latest_link="${destination_dir}/latest"
+log_location='/tmp/home_snapshot.log'
 
 #If a backup was taken today, don't take another one
 if [ -z $(ls "$destination_dir" | grep $today_date) ]
   then
   mkdir -p "${backup_path}"
-  rsync -aX --delete "$source_dir/" --link-dest "$latest_link" --exclude-from="$HOME/.config/home-snapshot-excl.conf" "$backup_path" 2> /tmp/backup_error.log
+  rsync -aX --delete --progress "$source_dir/" --link-dest "$latest_link" --exclude-from="$HOME/.config/home-snapshot-excl.conf" "$backup_path" 2> $log_location
 
   #If rsync doesn't exit with value 0 for any reason, notify the user
   rsync_exit=$?
   if [ $rsync_exit -ne "0" ]
   then
-    notify-send -a "rsync" -u critical 'Home snapshot possibly failed' "Check the log at /tmp/backup_error.log and run the service again."
+    notify-send -a "rsync" -u critical 'Home snapshot possibly failed' "Check the log at $log_location and run the service again."
+    echo "After you fix the issue, run systemctl --user start home-snapshot.service" >> $log_location
     rm -rf $backup_path
     exit 1
   fi
   rm -rf "${latest_link}"
   ln -rs "${backup_path}" "${latest_link}"
+else
+  echo "Snapshot was already taken: $today_date"
 fi
 
 #Cleanup old backups (keep only $max_snapshots)
